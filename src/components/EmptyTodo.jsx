8a88a1bd5c1b1a810bf4fe798044e958a8bdeae7
Elxan <elx4n13@gmail.com>
2022-08-13 01:34:49 +0400

    fix error

--- src/components/EmptyTodo.jsx
+++ src/components/EmptyTodo.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import image from '../images/image1.png'
+
 import '../styles/EmptyTodo.css'
 const EmptyTodo = () => {
   return (
     <div className='empty'>
-        <img src={image} alt="" srcset="" />
+        <img src={image} alt="Tapshiriq yoxdur" />
     </div>
   )
 }
